72bff2d6fdd035787ebd83fd22f99c7771427381
Mengjie Li <rned725@gmail.com>
2020-02-09 15:55:47 -0800

    commit

--- testBP/testBp.cpp
+++ testBP/testBp.cpp
@@ -14,7 +14,7 @@
 using namespace std;
 
 /*global variables*/
-int array_size = 1 << 17;
+int array_size = 1 << 21;
 int * array;
 int * log;
 
@@ -22,23 +22,22 @@ int * log;
 void warmup(int array_size) {
 	array = (int *)malloc(array_size * sizeof(int));
 	log = (int *)malloc(2 * array_size * sizeof(int));
-	int n, p;
 	int i;
+	int n, p;
 	n = rand();
 	p = rand();
 	for (i = 0; i < array_size; ++i) {
-
-
-		_mm_stream_si32(&log[2 * i], n);
-		_mm_stream_si32(&log[2 * i + 1], p);
+		log[2 * i] = n;
+		log[2 * i + 1] = p;
 		asm volatile("sfence");
-	//	mcsim_log_begin();
-//		log[2 * i] = n;
-//		log[2 * i + 1] = p;
+
+//		mcsim_log_begin();
+		//log[2 * i] = n;
+		//log[2 * i + 1] = p;
 		
-	//	mcsim_mem_fence();
-	//	mcsim_log_end();
-	//	mcsim_mem_fence();
+//		mcsim_mem_fence();
+//		mcsim_log_end();
+//		mcsim_mem_fence();
 
 		array[i] = n;
 	}
@@ -50,49 +49,50 @@ void run(int array_size) {
 	tot_start, tot_end, tot_cycles = 0, min_tot_cycles = uint64_t(-1);
 	int i, j, k;
 	int n, p;
+	int elt_size = 1;
 	//repeat multiple times
 	for (j = 0; j < 100; ++j) {
 		n = rand();
 		p = rand();
 		tot_start = rdtsc();
-		for (i = 0; i < array_size; ++i) {
-			//tot_start = rdtsc();
+		for (i = 0; i < array_size / elt_size; ++i) {
 			log_start = rdtsc();
-			//	n = 0;
-			//	p = 1;
 
-		    //    if (i % 1024 == 0)
-		    //    {
-		    //      mcsim_log_begin();
-		    //    }
-		    //      log[2 * i] = n;
-		    //      log[2 * i + 1] = p;
-			_mm_stream_si32(&log[2 * i], n);
+//			_mm_stream_si32(&log[2 * i * elt_size + k], n);
+//			_mm_stream_si32(&log[2 * i * elt_size + k + 1], p);
+//			asm volatile("mfence");
+
+//        mcsim_log_begin();
+			_mm_stream_si32(&log[2 * i + 0], n);
 			_mm_stream_si32(&log[2 * i + 1], p);
-		    //    if (i % 1 == 0)
-		    //    {
-		    //      mcsim_mem_fence();
-		    //      mcsim_log_end();
-		    //      mcsim_mem_fence();
-		    //    }
+		//	_mm_stream_si32(&log[2 * i + 2], n);
+		//	_mm_stream_si32(&log[2 * i + 3], p);
+		//	_mm_stream_si32(&log[2 * i + 4], n);
+		//	_mm_stream_si32(&log[2 * i + 5], p);
+		//	_mm_stream_si32(&log[2 * i + 6], n);
+		//	_mm_stream_si32(&log[2 * i + 7], p);
+		//	_mm_stream_si32(&log[2 * i + 8], n);
+		//	_mm_stream_si32(&log[2 * i + 9], p);
+		//	_mm_stream_si32(&log[2 * i + 10], n);
+		//	_mm_stream_si32(&log[2 * i + 11], p);
+		//	_mm_stream_si32(&log[2 * i + 12], n);
+		//	_mm_stream_si32(&log[2 * i + 13], p);
+		//	_mm_stream_si32(&log[2 * i + 14], n);
+		//	_mm_stream_si32(&log[2 * i + 15], p);
 			asm volatile("sfence");
 			log_end = rdtsc();
 			log_cycles = log_end - log_start;
 			if (log_cycles < min_log_cycles)
 				min_log_cycles = log_cycles;
 
-			//mcsim_log_begin();
-			//log[2 * i * elt_size + k] = n;
-			//log[2 * i * elt_size + 1 + k] = p;
-			//mcsim_mem_fence();
-			//mcsim_log_end();
-			//mcsim_mem_fence();
-	      
-			array[i] = n;
-			//tot_end = rdtsc();
-			//tot_cycles = tot_end - tot_start;
-			//if (tot_cycles < min_tot_cycles)
-			//	min_tot_cycles = tot_cycles;
+			array[i * elt_size + 0] = n;
+		//	array[i * elt_size + 1] = n;
+		//	array[i * elt_size + 2] = n;
+		//	array[i * elt_size + 3] = n;
+		//	array[i * elt_size + 4] = n;
+		//	array[i * elt_size + 5] = n;
+		//	array[i * elt_size + 6] = n;
+		//	array[i * elt_size + 7] = n;
 		}
 		tot_end = rdtsc();
 		tot_cycles = tot_end - tot_start;
@@ -102,7 +102,6 @@ void run(int array_size) {
 
 	printf("bypass: log_cycles = %lu \n", min_log_cycles);
 	printf("bypass: tot_cycles = %lu \n", min_tot_cycles);
-
 }
 
 
@@ -113,7 +112,6 @@ int main() {
 	//warmup
 	warmup(array_size);
 
-	//run
 	//CALLGRIND_START_INSTRUMENTATION;
 	run(array_size);
 	//CALLGRIND_STOP_INSTRUMENTATION;
--- testBP/testCc.cpp
+++ testBP/testCc.cpp
@@ -1,9 +1,8 @@
 //various elt size
 
-
-#include <iostream>
 /*#include <stdio.h>*/
 /*#include <stdlib.h>*/
+#include <iostream>
 #include <emmintrin.h>
 #include <time.h>
 /*#include <unistd.h>*/
@@ -12,11 +11,12 @@
 #include "helper.h"
 #include "defines.h"
 
+using namespace std;
+
 /*global variables*/
-int array_size = 1 << 17;
+int array_size = 1 << 21;
 int * array;
 int * log;
-
 
 
 void warmup(int array_size) {
@@ -27,8 +27,6 @@ void warmup(int array_size) {
 	n = rand();
 	p = rand();
 	for (i = 0; i < array_size; ++i) {
-
-
 		log[2 * i] = n;
 		log[2 * i + 1] = p;
 		asm volatile("sfence");
@@ -51,45 +49,67 @@ void run(int array_size) {
 	tot_start, tot_end, tot_cycles = 0, min_tot_cycles = uint64_t(-1);
 	int i, j, k;
 	int n, p;
+	int elt_size = 8;
 	//repeat multiple times
 	for (j = 0; j < 100; ++j) {
 		n = rand();
 		p = rand();
 		tot_start = rdtsc();
-		for (i = 0; i < array_size; ++i) {
-		//	tot_start = rdtsc();
+		for (i = 0; i < array_size / elt_size; ++i) {
 			log_start = rdtsc();
-			//n = -1;
-			//p = -2;
 
-			//if (i % 1 != 0)
-			{
-			  log[2 * i] = n;
-			  log[2 * i + 1] = p;
-//			  _mm_clflush(&log[2 * i]);
-//			  _mm_clflush(&log[2 * i + 1]);
-			  _mm_clflush(&log[2 * i]);
-			  _mm_clflush(&log[2 * i + 1]);
-			}
+//			_mm_stream_si32(&log[2 * i * elt_size + k], n);
+//			_mm_stream_si32(&log[2 * i * elt_size + k + 1], p);
+//			asm volatile("mfence");
+
+//        mcsim_log_begin();
+			log[2 * i * elt_size + 0] = n;
+			log[2 * i * elt_size + 1] = p;
+			log[2 * i * elt_size + 2] = n;
+			log[2 * i * elt_size + 3] = p;
+			log[2 * i * elt_size + 4] = n;
+			log[2 * i * elt_size + 5] = p;
+			log[2 * i * elt_size + 6] = n;
+			log[2 * i * elt_size + 7] = p;
+			log[2 * i * elt_size + 8] = n;
+			log[2 * i * elt_size + 9] = p;
+			log[2 * i * elt_size + 10] = n;
+			log[2 * i * elt_size + 11] = p;
+			log[2 * i * elt_size + 12] = n;
+			log[2 * i * elt_size + 13] = p;
+			log[2 * i * elt_size + 14] = n;
+			log[2 * i * elt_size + 15] = p;
+			asm volatile("sfence");
+			_mm_clflush(&log[2 * i * elt_size + 0]);
+			_mm_clflush(&log[2 * i * elt_size + 1]);
+			_mm_clflush(&log[2 * i * elt_size + 2]);
+			_mm_clflush(&log[2 * i * elt_size + 3]);
+			_mm_clflush(&log[2 * i * elt_size + 4]);
+			_mm_clflush(&log[2 * i * elt_size + 5]);
+			_mm_clflush(&log[2 * i * elt_size + 6]);
+			_mm_clflush(&log[2 * i * elt_size + 7]);
+			_mm_clflush(&log[2 * i * elt_size + 8]);
+			_mm_clflush(&log[2 * i * elt_size + 9]);
+			_mm_clflush(&log[2 * i * elt_size + 10]);
+			_mm_clflush(&log[2 * i * elt_size + 11]);
+			_mm_clflush(&log[2 * i * elt_size + 12]);
+			_mm_clflush(&log[2 * i * elt_size + 13]);
+			_mm_clflush(&log[2 * i * elt_size + 14]);
+			_mm_clflush(&log[2 * i * elt_size + 15]);
 			asm volatile("sfence");
 			log_end = rdtsc();
 			log_cycles = log_end - log_start;
 			if (log_cycles < min_log_cycles)
 				min_log_cycles = log_cycles;
 
-//			mcsim_log_begin();
-			//log[2 * i] = n;
-			//log[2 * i + 1] = p;
-			
-//			mcsim_mem_fence();
-//			mcsim_log_end();
-//			mcsim_mem_fence();
-
-			array[i] = n;
-			//tot_end = rdtsc();
-			//tot_cycles = tot_end - tot_start;
-			//if (tot_cycles < min_tot_cycles)
-			//	min_tot_cycles = tot_cycles;
+			array[i * elt_size + 0] = n;
+			array[i * elt_size + 1] = n;
+			array[i * elt_size + 2] = n;
+			array[i * elt_size + 3] = n;
+			array[i * elt_size + 4] = n;
+			array[i * elt_size + 5] = n;
+			array[i * elt_size + 6] = n;
+			array[i * elt_size + 7] = n;
 		}
 		tot_end = rdtsc();
 		tot_cycles = tot_end - tot_start;
@@ -99,8 +119,8 @@ void run(int array_size) {
 
 	printf("cached: log_cycles = %lu \n", min_log_cycles);
 	printf("cached: tot_cycles = %lu \n", min_tot_cycles);
-
 }
+
 
 
 int main() {
@@ -109,11 +129,18 @@ int main() {
 	//warmup
 	warmup(array_size);
 
-
 	//CALLGRIND_START_INSTRUMENTATION;
 	run(array_size);
 	//CALLGRIND_STOP_INSTRUMENTATION;
 	//CALLGRIND_DUMP_STATS;
 
+
 	return 0;
 }
+
+
+
+		
+
+
+
